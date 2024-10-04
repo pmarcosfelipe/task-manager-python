63e70ead95c55cc8e04d2c5f7976c914a88c7730
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-04 09:16:56 +0100

    created function create_task

--- manager.py
+++ manager.py
@@ -1,7 +1,18 @@
+def create_task(tasks, name):
+  task = {
+    "name": name,
+    "completed": False
+  }
+
+  tasks.append(task)
+  print(f"Task {name} was successfully created...")
+  return
+
+tasks = [];
 while True:
   print("\nMenu Task Manager: ")
-  print("1. Add Task")
-  print("2. Open Task")
+  print("1. Create Task")
+  print("2. Show Tasks")
   print("3. Update Task")
   print("4. Complete Task")
   print("5. Delete Complete Task")
@@ -9,7 +20,11 @@ while True:
 
   choose = input("Choose an option: ")
 
-  if choose == "6":
+  if choose == "1":
+    task_name = input("Type task name: ")
+    create_task(tasks, task_name)
+    break
+  elif choose == "6":
     break
 
   print("Program finalized.")
