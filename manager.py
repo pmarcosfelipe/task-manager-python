3617f9492407b34a791f1aa3831f4388e0c34001
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-04 06:02:57 +0100

    created initial menu

---
+++ manager.py
@@ -0,0 +1,15 @@
+while True:
+  print("\nMenu Task Manager: ")
+  print("1. Add Task")
+  print("2. Open Task")
+  print("3. Update Task")
+  print("4. Complete Task")
+  print("5. Delete Complete Task")
+  print("6. Quit")
+
+  choose = input("Choose an option: ")
+
+  if choose == "6":
+    break
+
+  print("Program finalized.")
